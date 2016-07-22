56c1be18ac0e77eb709b5d88ed53d9e5e1e8d79d
bkmxer <ilchuk_anton@mail.ru>
2016-07-22 17:35:33 +0300

    beta

--- js/common.js
+++ js/common.js
@@ -7,13 +7,68 @@ function randomInteger(min, max) {
 }
 
 
-setInterval(function ScrollVariants(){
-var i = randomInteger(0,2);
-	document.getElementById('Moto_append').innerHTML = variants_array[i];
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
 
-}, 4000);
 
 
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
+
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".stick-header").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".stick-header").removeClass("active");
+    }
+});
+
+//Scrollers to the anchors
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
 
 
 	//Цели для Яндекс.Метрики и Google Analytics
